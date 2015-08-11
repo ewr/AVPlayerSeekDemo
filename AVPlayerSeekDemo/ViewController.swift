df06dcd118a7009d84f92939f7b3646cafeda23b
Eric Richardson <e@ericrichardson.com>
2015-08-11 17:49:06 -0400

    Initial Commit

---
+++ AVPlayerSeekDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AVPlayerSeekDemo
+//
+//  Created by Eric Richardson on 8/11/15.
+//  Copyright © 2015 Eric Richardson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
